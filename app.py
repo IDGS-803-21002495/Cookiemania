ab29939970cf794f68c7470f6e4677ab2b43eddb
IDGS-803-22002362 <81542@alumnos.utleon.edu.mx>
2025-03-24 14:06:41 -0600

    Generacion de base de datos final

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_wtf.csrf import CSRFProtect
+from flask import g
+import os
+from models import db
+from config import DevelopmentConfig
 
 app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+csrf = CSRFProtect(app)
 
 @app.route("/")
 @app.route("/index")
@@ -8,4 +15,9 @@ def index():
 	return render_template("index.html")
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	csrf.init_app(app)
+	db.init_app(app)
+
+	with app.app_context():
+		db.create_all()
+	app.run()
