9a4c80a5b1ae0e45c4f0c3d41f3440d78f7a8564
IDGS-803-22002335 <tovardiazjuanaesmeralda@gmail.com>
2025-03-24 19:26:46 -0600

    Esmeralda_Frontend_inventarioProduccion

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/inventarioProduccion")
+def prueba():
+	return render_template("inventarioProducto.html")
+
 if __name__ == '__main__':
 	app.run(debug=True)
