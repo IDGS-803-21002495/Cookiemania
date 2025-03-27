02dc32ba6ca616bf8d5845ee13e753ca7e2ee90a
IDGS-803-22002362 <81542@alumnos.utleon.edu.mx>
2025-03-26 22:55:28 -0600

    Alberto_Rosas_Login_verificacion2stepsV1

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_wtf.csrf import CSRFProtect
 from flask import g
@@ -13,11 +14,77 @@ csrf = CSRFProtect(app)
 @app.route("/index")
 def index():
 	return render_template("index.html")
+=======
+from flask import Flask, redirect, url_for, render_template
+from flask_mail import Mail
+from models import db
+from models.usuario import Usuario  
+from config import DevelopmentConfig, mail
+from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager
 
+from blueprints.auth import auth_bp
+from blueprints.compras import compras_bp
+from blueprints.inventario import inventario_bp
+from blueprints.pedidos import pedidos_bp
+from blueprints.produccion import produccion_bp
+from blueprints.proveedores import proveedores_bp
+from blueprints.recetas import recetas_bp
+from blueprints.usuarios import usuarios_bp
+from blueprints.ventas import ventas_bp
+
+app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+
+csrf = CSRFProtect(app)
+
+db.init_app(app)
+
+# Inicializamos Flask-Mail
+mail.init_app(app)
+
+# Configuracion Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'auth.login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Usuario.query.get(int(user_id))
+
+# Registro de blueprints
+app.register_blueprint(auth_bp)
+app.register_blueprint(compras_bp)
+app.register_blueprint(inventario_bp)
+app.register_blueprint(pedidos_bp)
+app.register_blueprint(produccion_bp)
+app.register_blueprint(proveedores_bp)
+app.register_blueprint(recetas_bp)
+app.register_blueprint(usuarios_bp)
+app.register_blueprint(ventas_bp)
+
+# Rutas generales
+@app.route("/")
+def home():
+    return redirect(url_for("index"))
+>>>>>>> Stashed changes
+
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+# 7. Arranque de la aplicación
 if __name__ == '__main__':
+<<<<<<< Updated upstream
 	csrf.init_app(app)
 	db.init_app(app)
 
 	with app.app_context():
 		db.create_all()
 	app.run()
+=======
+    with app.app_context():
+        db.create_all()  
+
+    app.run(debug=True)
+>>>>>>> Stashed changes
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import os 
 from sqlalchemy import create_engine
 import urllib
@@ -10,3 +11,29 @@ class DevelopmentConfig(Config):
         DEBUG=True
         SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:270901@127.0.0.1:3306/cookiemania'
         SQLALCHEMY_TRACK_MODIFICATIONS=False
+=======
+import os
+import urllib
+from flask_mail import Mail
+
+# Instancia de Flask-Mail
+mail = Mail()
+
+class Config(object):
+    SECRET_KEY = 'CookiemaniaSecret'
+    SESSION_COOKIE_SECURE = False
+
+    # Configuracion  Flask-Mail
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'luisrosasbocanegra@gmail.com'
+    MAIL_PASSWORD = 'mzoj owgd eqvm zgai'
+
+    MAIL_DEFAULT_SENDER = ('Galletamaniaco', 'luisrosasbocanegra@gmail.com')
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:270901@127.0.0.1/cookiemania'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+>>>>>>> Stashed changes
